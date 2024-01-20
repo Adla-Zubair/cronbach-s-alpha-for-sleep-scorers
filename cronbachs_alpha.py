be320b59c44ff2673e62199bb5de39ca2b078c24
Adla Zubair <adlazubair2@gmail.com>
2024-01-20 11:33:47 +0530

    updated code

--- cronbachs_alpha.py
+++ cronbachs_alpha.py
@@ -50,17 +50,20 @@ stage_ratios = pd.DataFrame()
 for i in range(len(eegfiles)):
     try:
         
+        # Loadin theraw eeg file
         raw = mne.io.read_raw_edf(eegfiles[i] , preload = False, verbose = True)
         
         fname = os.path.basename(eegfiles[i])[:-4]
         
+        # Loading the scored hypnograms
         hypno_k = np.loadtxt(f'{fname}_0001_reduced_krishan.csv',dtype = str)
         hypno_a = np.loadtxt(f'{fname}_0001_reduced_scoredbyadla.csv',dtype = str)
         
+        # Scoring through yasa 
         sls = yasa.SleepStaging(raw, eeg_name = "EEG CZ-Ref")
         hypno_y = sls.predict()
         
-        
+        # Calculating the percent of each sleep stage wrt to total number of sleep stages for k
         k_stage_w = pd.DataFrame(hypno_k).value_counts()['W']*100/ pd.DataFrame(hypno_k).count()
         k_stage_n1 = pd.DataFrame(hypno_k).value_counts()['N1']*100/ pd.DataFrame(hypno_k).count()
         k_stage_n2 = pd.DataFrame(hypno_k).value_counts()['N2']*100/ pd.DataFrame(hypno_k).count()
@@ -68,27 +71,27 @@ for i in range(len(eegfiles)):
         k_stage_r = pd.DataFrame(hypno_k).value_counts()['R']*100/ pd.DataFrame(hypno_k).count()
     
         
-        
+        # Calculating the percent of each sleep stage wrt to total number of sleep stages for a
         a_stage_w = pd.DataFrame(hypno_a).value_counts()['W']*100/ pd.DataFrame(hypno_a).count()
         a_stage_n1 = pd.DataFrame(hypno_a).value_counts()['N1']*100/ pd.DataFrame(hypno_a).count()
         a_stage_n2 = pd.DataFrame(hypno_a).value_counts()['N2']*100/ pd.DataFrame(hypno_a).count()
         a_stage_n3 = pd.DataFrame(hypno_a).value_counts()['N3']*100/ pd.DataFrame(hypno_a).count()
         a_stage_r = pd.DataFrame(hypno_a).value_counts()['R']*100/ pd.DataFrame(hypno_a).count()
         
-        
+        # Calculating the percent of each sleep stage wrt to total number of sleep stages for yasa
         y_stage_w = pd.DataFrame(hypno_y).value_counts()['W']*100/ pd.DataFrame(hypno_y).count()
         y_stage_n1 = pd.DataFrame(hypno_y).value_counts()['N1']*100/ pd.DataFrame(hypno_y).count()
         y_stage_n2 = pd.DataFrame(hypno_y).value_counts()['N2']*100/ pd.DataFrame(hypno_y).count()
         y_stage_n3 = pd.DataFrame(hypno_y).value_counts()['N3']*100/ pd.DataFrame(hypno_y).count()
         y_stage_r = pd.DataFrame(hypno_y).value_counts()['R']*100/ pd.DataFrame(hypno_y).count()
     
-    
+        # Saving these values into a list
         stage_ratios_k =  [k_stage_w, k_stage_n1, k_stage_n2, k_stage_n3, k_stage_r]
         stage_ratios_a =  [a_stage_w, a_stage_n1, a_stage_n2, a_stage_n3, a_stage_r]
         stage_ratios_y =  [y_stage_w, y_stage_n1, y_stage_n2, y_stage_n3, y_stage_r]
     
         
-    
+        # Stage ratios saved into a mastersheet with subject name stagewise
         stage_ratios = pd.concat([pd.DataFrame(stage_ratios_a), pd.DataFrame(stage_ratios_k), pd.DataFrame(stage_ratios_y)], axis = 1)
         stage_ratios['subjname'] = fname
         mastersheet = mastersheet._append(stage_ratios)
